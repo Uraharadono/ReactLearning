33e115dd863652c501157a28bc82749e0f77e647
Uraharadono <35668552+Uraharadono@users.noreply.github.com>
2018-10-31 17:45:16 +0100

    Module 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
-    ]);
-}
+
+
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
 
 class App extends React.Component {
-    handleTitleClick() {
-        alert("You clicked title.")
-    }
-    render() {
-        return React.createElement("div", { id: "neki-id", onClick: this.handleTitleClick }, [
-            React.createElement("h1", {}, "Adopt me!"),
-            React.createElement(Pet, { name: "Lujka", animal: "Dog", breed: "Avlijaner" }),
-            React.createElement(Pet, { name: "Shanequa", animal: "Cat", breed: "Jos gori avlijaner" }),
-            React.createElement(Pet, { name: "Konjo", animal: "Horse", breed: "Travnati avlijaner" })
-        ]);
-    }
+  handleTitleClick() {
+    alert("You clicked title.");
+  }
+  render() {
+    return React.createElement(
+      "div",
+      { id: "neki-id", onClick: this.handleTitleClick },
+      [
+        React.createElement("h1", {}, "Adopt me!"),
+        React.createElement(Pet, {
+          name: "Lujka",
+          animal: "Dog",
+          breed: "Avlijaner"
+        }),
+        React.createElement(Pet, {
+          name: "Shanequa",
+          animal: "Cat",
+          breed: "Jos gori avlijaner"
+        }),
+        React.createElement(Pet, {
+          name: "Konjo",
+          animal: "Horse",
+          breed: "Travnati avlijaner"
+        })
+      ]
+    );
+  }
 }
 
 // const App = () => {
@@ -30,4 +48,4 @@ class App extends React.Component {
 //     ]);
 // }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
