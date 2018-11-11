6d782802ab552f295df0f41e01b9d2b784c6886e
Uraharadono <35668552+Uraharadono@users.noreply.github.com>
2018-11-11 21:16:00 +0100

    Module 6 - Rendering data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import React from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pets: []
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet.find({ output: "full", location: "New York, NY" })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets != null && data.petfinder.pets.pet != null) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          }
+          else {
+            pets = [data.petfinder.pets.pet];
+          }
+        }
+        else {
+          pets = [];
+        }
+
+        this.setState({
+          pets: pets
+          // moze i ovako jer su iste rijeci, ali je meni sranje 
+          // pets
+        })
+      })
+  }
+
+
   // handleTitleClick() {
   //   alert("You clicked title.");
   // }
@@ -37,9 +75,25 @@ class App extends React.Component {
       */
       <div>
         <h1>Adopt me</h1>
-        <Pet name="Lujka" animal="Dog" breed="Avlijaner"></Pet>
-        <Pet name="Shanequa" animal="Cat" breed="Jos gori avlijaner"></Pet>
-        <Pet name="Konjo" animal="Horse" breed="Travnati avlijaner"></Pet>
+        <div>
+          {
+            this.state.pets.map(pet => {
+
+              let breed;
+
+              if (Array.isArray(pet.breeds.breed)) {
+                breed = pet.breeds.breed.join(", ");
+              }
+              else {
+                breed = pet.breeds.breed
+              }
+              return (
+                <Pet key={pet.id} animal={pet.animal} name={pet.name} breed={breed} media={pet.media} location={`${pet.contact.city}, ${pet.contact.state}`} />
+              )
+            })
+          }
+        </div>
+
       </div>
     )
   }
--- src/Pet.js
+++ src/Pet.js
@@ -1,21 +1,46 @@
 import React from "react";
 
-const Pet = props => {
-  // with pure js
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, props.name),
-  //   React.createElement("h2", {}, props.animal),
-  //   React.createElement("h2", {}, props.breed)
-  // ]);
+// const Pet = props => {
+//   // with pure js
+//   // return React.createElement("div", {}, [
+//   //   React.createElement("h1", {}, props.name),
+//   //   React.createElement("h2", {}, props.animal),
+//   //   React.createElement("h2", {}, props.breed)
+//   // ]);
 
-  // this is jsx stuff
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  )
-};
+//   // this is jsx stuff
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h2>{props.animal}</h2>
+//       <h2>{props.breed}</h2>
+//     </div>
+//   )
+// };
+
+class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+    let photos = [];
+
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>{animal} - {breed} - {location}</h2>
+        </div>
+
+      </div>
+    )
+  }
+}
 
 export default Pet;
