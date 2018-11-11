2c823cc18137b5ab4193bac23ac65b0f0a30377d
Uraharadono <35668552+Uraharadono@users.noreply.github.com>
2018-11-11 17:34:18 +0100

    Module 5 - JSX

--- src/App.js
+++ src/App.js
@@ -3,32 +3,45 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("You clicked title.");
-  }
+  // handleTitleClick() {
+  //   alert("You clicked title.");
+  // }
   render() {
-    return React.createElement(
-      "div",
-      { id: "neki-id", onClick: this.handleTitleClick },
-      [
-        React.createElement("h1", {}, "Adopt me!"),
-        React.createElement(Pet, {
-          name: "Lujka",
-          animal: "Dog",
-          breed: "Avlijaner"
-        }),
-        React.createElement(Pet, {
-          name: "Shanequa",
-          animal: "Cat",
-          breed: "Jos gori avlijaner"
-        }),
-        React.createElement(Pet, {
-          name: "Konjo",
-          animal: "Horse",
-          breed: "Travnati avlijaner"
-        })
-      ]
-    );
+    // return React.createElement(
+    //   "div",
+    //   { id: "neki-id", onClick: this.handleTitleClick },
+    //   [
+    //     React.createElement("h1", {}, "Adopt me!"),
+    //     React.createElement(Pet, {
+    //       name: "Lujka",
+    //       animal: "Dog",
+    //       breed: "Avlijaner"
+    //     }),
+    //     React.createElement(Pet, {
+    //       name: "Shanequa",
+    //       animal: "Cat",
+    //       breed: "Jos gori avlijaner"
+    //     }),
+    //     React.createElement(Pet, {
+    //       name: "Konjo",
+    //       animal: "Horse",
+    //       breed: "Travnati avlijaner"
+    //     })
+    //   ]
+    // );
+    return (
+      /* Posto u reactu mora se uvijek sve wrapat u div kad je return u pitanju mogu staviti umjesto div-a 
+      <React.Fragment>
+      ..sadrzaj
+      </React.Fragment>
+      */
+      <div>
+        <h1>Adopt me</h1>
+        <Pet name="Lujka" animal="Dog" breed="Avlijaner"></Pet>
+        <Pet name="Shanequa" animal="Cat" breed="Jos gori avlijaner"></Pet>
+        <Pet name="Konjo" animal="Horse" breed="Travnati avlijaner"></Pet>
+      </div>
+    )
   }
 }
 
@@ -44,4 +57,8 @@ class App extends React.Component {
 
 // ovo dvoje je isto
 // ReactDOM.render(React.createElement(App), document.getElementById("root"));
-render(React.createElement(App), document.getElementById("root"));
+
+// ovo je ako ne koristim jsx, vec pure js
+// render(React.createElement(App), document.getElementById("root"));
+// ovo je ako koristim jsx
+render(<App></App>, document.getElementById("root"));
