1eb7f8a47ceb69b51ae7efb176cf99d3debeb458
Uraharadono <35668552+Uraharadono@users.noreply.github.com>
2018-11-12 23:28:00 +0100

    Module 8 - Async UI pt1. Error: Cannot read property 'key' of undefined

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,72 @@
 import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router"
 
+const petfinder = pf({
+    key: "130015cd42380eabf21cb2b04a374cb9",
+    secret: "e44e30dce18764e5823ce61bb8f718b6"
+});
 
 class Details extends React.Component {
-    render (){
-        return <h1>MADAFAKAAAAAAAAAA!</h1>
+    // ovo je onaj novi fazon sto je on pokazao, stage 3 ili 5, top level class property
+    state = {
+        loading: true
+    };
+
+    // ovo je kako meni treba da koristim
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = {
+    //         loading: true
+    //     };
+    // }
+
+    componentDidMount() {
+        petfinder.pet.get({
+            output: "full",
+            id: this.props.id
+        }).then((data) => {
+            const pet = data.petfinder.pet;
+            let breed;
+            if (Array.isArray(pet.breeds.breed)) {
+                breed = pet.breeds.breed.join(", ");
+            }
+            else {
+                breed = pet.breeds.breed;
+            }
+            this.setState({
+                name: pet.name,
+                anima: pet.animal,
+                location: `${pet.contact.city}, ${pet.contact.state}`,
+                description: pet.description,
+                media: pet.media,
+                breed: breed,
+                loading: false
+            })
+        }).catch(err => {
+            this.setState({ error: err });
+            navigate("/");
+        }
+        );
+    }
+
+    render() {
+        // return <h1>MADAFAKAAAAAAAAAA!</h1>
+
+        if (this.state.loading) {
+            return <h1>loading...</h1>
+        }
+        else {
+            const { name, animal, breed, location, description } = this.state;
+            return <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{animal} - {breed} - {location}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+        }
     }
 }
 
